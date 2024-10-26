96c033aaeda30dc4740a27ddcae9371fb1e3a287
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-10-26 17:31:12 +0530

    intialized express

---
+++ backend/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const jwtScret = "hello iam mohan"
+
+router.post('/signup',(req,res)=>{
+    const{ username , email , password , role} = req.body
+    const hashedPassword = 
+})
+
+module.exports = router
